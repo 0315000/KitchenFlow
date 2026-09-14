7c7b4e8d1b8c5947661ece6c69059e5a6cd917cf
DESKTOP-5KMK44C\ysjan <yseo031500@gmail.com>
2026-09-14 16:30:56 +0900

    Update InventoryItem.cs

---
+++ src/KitchenFlow.Api/Models/InventoryItem.cs
@@ -0,0 +1,11 @@
+namespace KitchenFlow.Api.Models
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; } = string.Empty;
+        public decimal Quantity { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public decimal Threshold { get; set; }
+    }
+}
