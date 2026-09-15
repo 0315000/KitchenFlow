26ee6a6da4d0144d8aaff241176bcb5e3bc458d3
DESKTOP-5KMK44C\ysjan <yseo031500@gmail.com>
2026-09-15 17:22:16 +0900

    Add CookingTask Table

--- src/KitchenFlow.Api/Migrations/20260915081723_AddCookingTask.Designer.cs
+++ src/KitchenFlow.Api/Migrations/20260915081723_AddCookingTask.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using KitchenFlow.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KitchenFlow.Api.Migrations
 {
     [DbContext(typeof(KitchenFlowDbContext))]
-    partial class KitchenFlowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260915081723_AddCookingTask")]
+    partial class AddCookingTask
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.12");
