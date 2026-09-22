e398026440f80d800ddbb3923a272ce2319540d2
DESKTOP-5KMK44C\ysjan <yseo031500@gmail.com>
2026-09-22 15:13:12 +0900

    update ingredient.cs

---
+++ src/KitchenFlow.Api/Models/Ingredient.cs
@@ -0,0 +1,7 @@
+public class Ingredient
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Unit { get; set; } = string.Empty;
+    public decimal StockQty { get; set; }
+}
